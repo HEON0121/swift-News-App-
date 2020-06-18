cd455b1a46d89f460f376bd97528a8509746edef
HEON0121 <seheon.emma@gmail.com>
2020-06-18 14:26:42 +0900

    Initial Commit

---
+++ sentiTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sentiTable
+//
+//  Created by 김세헌 on 18/6/20.
+//  Copyright © 2020 김세헌. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

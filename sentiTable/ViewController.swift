6a108c0da9a14844c75b427ec91e59208b0bf02c
HEON0121 <seheon.emma@gmail.com>
2020-06-20 12:56:24 +0900

    ui error

--- sentiTable/ViewController.swift
+++ sentiTable/ViewController.swift
@@ -11,7 +11,37 @@ import UIKit
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var TableViewMain: UITableView!
+    // 1. http 통신 방법 - urlsession
+    // 2. 뉴스 데이터 - JSON 데이터 형태
     
+    //500회 무료
+    func getNews(){
+      let task = URLSession.shared.dataTask(with: URL(string :
+             "http://newsapi.org/v2/top-headlines?country=kr&apiKey=693738ceab2a4e258038d1bfd1beb09e")!) { (data, response, error) in
+                 //optional binding
+             if let dataJson = data {
+               
+                // 오류 상황 대처하는 do, catch
+                do {
+                    //JSON parsing 직렬화(serialization) // data - > json data
+                    //swift 문법 : json을 처리하는 Dictionary
+                    let json = try JSONSerialization.jsonObject(with: dataJson, options: []) as! Dictionary<String, Any>
+                    let articles = json["articles"] as! Array<Dictionary<String, Any>>
+                    //print(articles)
+                    for(i, value) in articles.enumerated(){
+                       if  let v = value as? Dictionary<String, Any> {
+                            print("\(v["title"])")
+                        }
+                    }
+                }
+                catch {}
+               
+             }
+                
+        }
+        task.resume()
+    
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //data 몇 개? 10 줄
         return 10
@@ -32,7 +62,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     //클릭
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        print("\(indexPath.row)번 클릭!!!")
     }
     
 
@@ -41,6 +71,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Do any additional setup after loading the view.
         TableViewMain.delegate=self
         TableViewMain.dataSource=self
+        
+        getNews()
     }
     //Table View 테이블로 된 뷰 - 
     
